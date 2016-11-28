c7ed90bfedaaf64c9b333d6075f88fae46794bf4
Carlos <beltrame@ieee.org>
2016-11-28 20:39:34 -0200

    criada a classe motor

--- fabrica.php
+++ fabrica.php
@@ -4,7 +4,6 @@
 require_once './Carro.php';
 require_once './Motor.php';
 
-
 $motor = new Motor();
 $veiculo1 = new Carro($motor , "Azul");
 //$veiculo2 = $veiculo1 ; // o objeto mantem o mesmo ID
@@ -13,12 +12,9 @@ $veiculo2 = clone $veiculo1;
 $veiculo1->cor = "Vermelho";
 $veiculo2->abastecer(15);
 
-
-
 var_dump($veiculo1);
 
 $veiculo1->acelerar(10);
-
 
 echo "Potencia do motor: ".Motor::POTENCIA;
 ?>
